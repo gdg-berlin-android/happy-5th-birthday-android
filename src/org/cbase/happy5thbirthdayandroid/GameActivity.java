1b5aefd71ec92c83f28e344dc355a39dafc908df
keyboardsurfer <keyboardsurfer@c-base.org>
2012-10-14 12:03:46 +0200

    GameActivity extends BaseGameActivity

--- src/org/cbase/happy5thbirthdayandroid/GameActivity.java
+++ src/org/cbase/happy5thbirthdayandroid/GameActivity.java
@@ -1,10 +1,13 @@
 package org.cbase.happy5thbirthdayandroid;
 
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class GameActivity extends Activity {
+public class GameActivity extends BaseGameActivity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -19,4 +22,27 @@ public class GameActivity extends Activity {
     return true;
   }
 
+  @Override
+  public EngineOptions onCreateEngineOptions() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void onCreateResources(OnCreateResourcesCallback arg0) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onCreateScene(OnCreateSceneCallback arg0) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onPopulateScene(Scene arg0, OnPopulateSceneCallback arg1) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
 }
